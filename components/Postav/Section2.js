e50898f7b57afb195332c0e20f361cd93ee7bd98
Ivan Brajković <Ivan@PC192.168.1.100>
2019-09-19 16:31:03 +0200

    add page stalni postav, responsive

--- components/Postav/Section2.js
+++ components/Postav/Section2.js
@@ -24,8 +24,8 @@ const Section2 = ({ lang, data }) => {
             );
             return img.toLowerCase().includes(item.src.toLowerCase());
           });
-          console.log("TCL: item.src", item.src);
           // console.log("TCL: large", large);
+          console.log("TCL: item.src", item.src);
           console.log("TCL: gallery", gallery);
           console.log("TCL: gallery.length", gallery.length);
 
