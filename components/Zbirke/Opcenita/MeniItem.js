8e07b000e848b38fa55481a2e71166332679e6c5
Ivan Brajković <ivan@PC192.168.1.100>
2019-09-27 19:48:33 +0200

    add detalji zvona

--- components/Zbirke/Opcenita/MeniItem.js
+++ components/Zbirke/Opcenita/MeniItem.js
@@ -1,16 +1,16 @@
 // Opcenita - section2 - meni item
 
-import Link from "next/link";
+import Link from 'next/link';
 
 const MeniItem = ({ index, page, item, style }) => (
   <Link
     key={index}
-    href="/zbirke/detalji/[zbirke]/[detalji]"
-    as={`/zbirke/detalji/${page}/${index}`}
+    href='/zbirke/detalji/[zbirke]/[detalji]'
+    as={`/zbirke/detalji/${page}/${index + 1}`}
   >
     <div style={style}>
-      <div className="w3-card-4 expand-on-hover">
-        <img className="img-cover" src={item} />
+      <div className='w3-card-4 expand-on-hover'>
+        <img className='img-cover' src={item} />
       </div>
     </div>
   </Link>
--- pages/zbirke/detalji/[zbirke]/[detalji]/index.js
+++ pages/zbirke/detalji/[zbirke]/[detalji]/index.js
@@ -1,24 +1,26 @@
 // Zbirke - detalji
 
-import { useEffect } from "react";
-// import Fade from "react-reveal/Fade";
+import { useEffect } from 'react';
 
-import fetchDataAsync from "../../../../../lib/fetchDataAsync";
-import Section1 from "../../../../../components/Zbirke/Detaljna/Section1";
-import Section2 from "../../../../../components/Zbirke/Detaljna/Section2";
-import Section3 from "../../../../../components/Zbirke/Detaljna/Section3";
+import fetchDataAsync from '../../../../../lib/fetchDataAsync';
+
+import Hero from '../../../../../components/Hero';
 
 const Detalji = ({ lang, data }) => {
-  const FOLDER = (data && data.folder) || "";
+  const folder = (data && data.folder) || '';
 
   const text = (data && data[lang]) || {};
-  const title = (text && text.title) || "";
-  const content = (text && text.content) || "";
+  const title = (text && text.title) || '';
+  const content1 = (text && text['content-1']) || '';
+  const content2 = (text && text['content-2']) || '';
 
-  const image = (data && data.image) || "";
-  const audio = (data && data.audio) || "";
-  const video = (data && data.video) || "";
-  const gallery = (data && data.gallery) || [];
+  const image = (data && data.image) || '';
+  const audio = (data && data.audio) || '';
+  // const video = (data && data.video) || '';
+  // const gallery = (data && data.gallery) || [];
+
+  const banners =
+    (data && data.banners && data.banners.map(item => folder + item)) || [];
 
   useEffect(() => {
     lightbox.reload();
@@ -26,21 +28,25 @@ const Detalji = ({ lang, data }) => {
   }, []);
 
   return (
-    <div className="container zbirke-detalji">
-      {/* <Fade delay={250} cascsade ssrReveal> */}
-      <Section1
-        folder={FOLDER}
-        title={title}
-        image={image}
-        audio={audio}
-        video={video}
-        data-aos="fade"
-        data-aos-duration="1000"
-      />
-      <Section2 content={content} />
-      <Section3 folder={FOLDER} data={gallery} />
-      {/* </Fade> */}
-    </div>
+    <>
+      <Hero title={text.hero} srcset={banners} />
+      <div
+        className='m-t-xs-20-xl-40 gap-xs-20-xl-40 container detalji-page'
+        data-aos='fade'
+        data-aos-duration='1000'
+      >
+        <div className='item-1'>
+          <img className='img-cover w3-card-4' src={`${folder}${image}`} />
+        </div>
+
+        <div className='item-2'>
+          <div className='content-1'>
+            <p>{content1}</p>
+            <p>{content2}</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -48,7 +54,7 @@ Detalji.getInitialProps = async context => {
   const zbirke = context.query.zbirke;
   const detalji = context.query.detalji;
   const params = [zbirke, detalji];
-  const data = await fetchDataAsync(context, "zbirke/detalji/", params);
+  const data = await fetchDataAsync(context, 'zbirke/detalji', params);
   return data;
 
   // Zbirke.getInitialProps = async context => {
