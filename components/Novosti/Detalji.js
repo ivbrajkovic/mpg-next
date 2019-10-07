2423766ee5119a78cb441caf5b55c882e776517c
ivbrajkovic <ivan.brajkovic@icloud.com>
2019-10-07 03:17:11 +0200

    add edukacija cms

--- components/Novosti/Detalji.js
+++ components/Novosti/Detalji.js
@@ -17,25 +17,22 @@ const Detalji = ({ data, onCloseDetalji }) => {
       data-aos-duration='1000'
     >
       <div className='w3-card-4 detaljan-prikaz'>
-        <h2 className='p-0-20 post-title content-1-light'>
+        <div className='p-0-20 post-title'>
           <div className='btnClose' onClick={() => onCloseDetalji()}>
-            X
+            <p>X</p>
           </div>
-          {title}
-        </h2>
-        <div className='p-xs-20-l-40'>
+          <p>{title}</p>
+        </div>
+        <div className='p-xs-20-xl-40'>
           <h1 className='header-1'>{header}</h1>
           <div className='text-center'>
             <img style={{ maxWidth: '100%' }} src={slika} />
           </div>
-          <div
-            className='m-t-xs-20-xl-40 content-1'
+          <p
+            className='m-t-xs-20-xl-40'
             dangerouslySetInnerHTML={{ __html: content }}
           />
-          <div
-            className='content-1'
-            dangerouslySetInnerHTML={{ __html: content2 }}
-          />
+          <p dangerouslySetInnerHTML={{ __html: content2 }} />
         </div>
       </div>
     </div>
